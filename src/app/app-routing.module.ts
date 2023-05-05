e0bff7bf3676bbb5869a2f27dd37e14acff02eb9
UtkarshaKarade <Utkarshakarade1996@gmail.com>
2023-05-05 13:46:17 +0530

    added design code for crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUsersComponent } from './components/add-users/add-users.component';
+import { EditUsersComponent } from './components/edit-users/edit-users.component';
+import { DeleteUsersComponent } from './components/delete-users/delete-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addUsers', component: AddUsersComponent },
+  { path: 'editUsers', component: EditUsersComponent },
+  { path: 'deleteUsers', component: DeleteUsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { NeoServiceService } from './neo-service.service';
-import {MatDatepickerModule} from '@angular/material/datepicker'
-import {MatInputModule} from '@angular/material/input'
-import {MatNativeDateModule} from '@angular/material/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import {MatButtonModule} from '@angular/material/button'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { DatePipe } from '@angular/common';
+import { AddUsersComponent } from './components/add-users/add-users.component';
+import { EditUsersComponent } from './components/edit-users/edit-users.component';
+import { DeleteUsersComponent } from './components/delete-users/delete-users.component';
+import { GetUsersComponent } from './components/get-users/get-users.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUsersComponent,
+    EditUsersComponent,
+    DeleteUsersComponent,
+    GetUsersComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,10 +37,9 @@ import { DatePipe } from '@angular/common';
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    DatePipe
-
+    DatePipe,
   ],
-  providers: [NeoServiceService,DatePipe],
-  bootstrap: [AppComponent]
+  providers: [NeoServiceService, DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
